bee9d417eaacbab5aec8b1697a0aa35e3ab807e4
nawalkanabee <nawalkanabee@gmail.com>
2022-10-30 18:30:46 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
+
+const button = document.getElementById("button");
+
 function addingEventListener() {
+  button.addEventListener("click", function () {
+    console.log("clicked");
+  });
 }
+
+
